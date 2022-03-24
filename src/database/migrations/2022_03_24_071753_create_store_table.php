35691665068c4e262c7633b991bd2fb877ce446c
higashitani0624 <higashitaniyu@crunchtimer.jp>
2022-03-24 23:31:55 +0900

    feat/database

---
+++ src/database/migrations/2022_03_24_071753_create_store_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStoreTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stores', function (Blueprint $table) {
+            $table->increments('id');//id
+            $table->string('review',100);//レビュー内容
+            $table->bigInteger('user_id')->unsigned();//ユーザーテーブルid
+            $table->timestamps();//タイムスタンプ
+
+            // 外部キーを設定する
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stores');
+    }
+};
---
+++ src/database/migrations/2022_03_24_075039_create_reviews_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 100);
+            $table->string('mail', 100);
+            $table->integer('gender');
+            $table->string('review',100);//レビュー内容
+            $table->integer('opinion');//評価値
+            $table->bigInteger('user_id')->unsigned();//ユーザid
+            $table->timestamps();
+
+            // 外部キーを設定する
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
