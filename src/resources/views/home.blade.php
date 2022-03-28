ab3afbde50a9f3f781c1b606fa90c930903c6239
Yuma Higashitani <94593301+higashitani0624@users.noreply.github.com>
2022-03-29 00:48:42 +0900

    Update home.blade.php

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -1,12 +1,1 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <title>todo</title>
-    </head>
-    <body>
-       <p class="text-gray-200">success</p>
-    </body>
-</html>
+
