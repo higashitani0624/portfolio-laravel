a279f40328bfef2deb46d25e10b246d3de4891f5
Yuma Higashitani <94593301+higashitani0624@users.noreply.github.com>
2022-03-29 00:50:10 +0900

    Update HomeController.php

--- src/app/Http/Controllers/HomeController.php
+++ src/app/Http/Controllers/HomeController.php
@@ -3,3 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $users = User::all();
+        return view('home',['users' => $users,]);
+    }
+}
