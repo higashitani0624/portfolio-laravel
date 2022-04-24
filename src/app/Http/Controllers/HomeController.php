d670c429d16426b899bccacc9d61f1ed48fb1021
Yuma Higashitani <higashitaniyu@crunchtimer.jp>
2022-04-24 16:11:32 +0900

    pagenation

--- src/app/Http/Controllers/HomeController.php
+++ src/app/Http/Controllers/HomeController.php
@@ -4,13 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        $users = User::all();
+        $users = DB::table('users')->paginate(3);
         return view('home',['users' => $users,]);
     }
 }
--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -14,6 +14,7 @@
             </div>
         </div>
         @endforeach
+        {{ $users -> links() }}
     </section>
 </div>
 @endsection
